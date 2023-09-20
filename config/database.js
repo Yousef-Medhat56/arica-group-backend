7a2cf68e32c42dfb0acaa00683a7bc2f027c283b
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-09-21 02:40:18 +0300

    update db

--- config/database.js
+++ config/database.js
@@ -1,26 +1,32 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+if (process.env.NODE_ENV == "development") {
+  module.exports = ({ env }) => ({
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "sqlite",
+      connection: {
+        filename: path.join(
+          __dirname,
+          "..",
+          env("DATABASE_FILENAME", ".tmp/data.db")
+        ),
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST'),
-//       port: env.int('DATABASE_PORT'),
-//       database: env('DATABASE_NAME'),
-//       user: env('DATABASE_USERNAME'),
-//       password: env('DATABASE_PASSWORD'),
-//       ssl: false
-//     },
-//     debug: false,
-//   },
-// });
+  });
+} else {
+  module.exports = ({ env }) => ({
+    connection: {
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: false,
+      },
+      debug: false,
+    },
+  });
+}
