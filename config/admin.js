04ec61eee3112434e0d85faf5a9c4b145b5d68d0
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-10-28 18:08:03 +0200

    update

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET',"6wTST32UqInNifjOzIgJlQ=="),
+    secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT',"5jok9K1kjhvf+YdjEr9nSQ=="),
+    salt: env('API_TOKEN_SALT'),
   },
 });
